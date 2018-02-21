9dcf34d484c71867ecd5c1091cea7076c709ab1a
Arnav Gupta <arnav@codingblocks.com>
2018-02-21 08:16:47 +0530

    better syntax
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/models/users.ts
+++ src/models/users.ts
@@ -5,39 +5,13 @@ import {createHandler} from '../handlers/sequelize'
 const Joi = jsonApi.Joi
 const sqlHandler = createHandler()
 
-export interface User {
-  id?: string
-  name: string
-}
-
 @DefineOn(jsonApi) @HandleWith(sqlHandler)
 @PrimaryKey('autoincrement') @ResourceName('users')
 class UserResource {
-  @required @attr
-  public id?: string
-  @required @attr
-  public name: string
-  @attr
-  public age: number
+  @required @attr public id?: string
+  @required @attr public name: string
+  @attr public age: number
 }
-
-// jsonApi.define<User>({
-//   namespace: 'json:api',
-//   resource: 'users',
-//   handlers: sqlHandler,
-//   primaryKey: 'autoincrement',
-//   attributes: {
-//     id: Joi.string(),
-//     name: Joi.string().required()
-//   },
-//   examples: [
-//     {type: 'users', name: 'Arnav'},
-//     {type: 'users', name: 'Prateek'},
-//     {type: 'users', name: 'Deepak'},
-//     {type: 'users', name: 'Rishab'},
-//     {type: 'users', name: 'Garima'}
-//   ]
-// })
 
 /**
  * @param force set to true only for dev mode (drops tables)
